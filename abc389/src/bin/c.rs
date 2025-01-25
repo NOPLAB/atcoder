1c24a420564e9d35607939a06e4e742a43a51e34
nop <noplab90@gmail.com>
2025-01-25 13:50:36 +0900

    tle c

--- abc389/src/bin/c.rs
+++ abc389/src/bin/c.rs
@@ -1,7 +1,9 @@
+use core::panic;
 #[allow(unused_imports)]
 use itertools::{iproduct, Itertools};
 #[allow(unused_imports)]
 use num_traits::pow;
+use proconio::input_interactive;
 #[allow(unused_imports)]
 use proconio::{
     fastout, input,
@@ -16,9 +18,37 @@ use std::iter::FromIterator;
 
 #[fastout]
 fn main() {
-    input!{
-        h: usize, w: usize,
-        s: [Chars; h],
-        mut plan: [(usize, usize, usize); h]
+    input_interactive! {
+        q: usize
+    }
+
+    let mut snake: VecDeque<usize> = VecDeque::new();
+
+    for _ in 0..q {
+        input_interactive! {
+            op: usize
+        }
+        match op {
+            1 => {
+                input_interactive! {
+                    l: usize
+                }
+
+                snake.push_back(l);
+            }
+            2 => {
+                snake.pop_front();
+            }
+            3 => {
+                input_interactive! {
+                    k: usize
+                }
+
+                let l: usize = snake.iter().take(k - 1).sum();
+
+                println!("{}", l);
+            }
+            _ => panic!(),
+        }
     }
 }
